61a6698cf56878fbbaf619eba32613a257c3ccf4
CommanderOslo <oslopony@gmail.com>
2017-12-20 02:46:07 +0300

    1.1, поменял местами M с N

--- homework1.f95
+++ homework1.f95
@@ -10,19 +10,19 @@ real(8), dimension(:), allocatable :: p
 real(8) :: max, summ
 real(8), dimension(:), allocatable :: smaximum
 integer(4), dimension(:), allocatable :: leftmaximum, rightmaximum, downmaximum
-N = size(a, dim = 1)
-M = size(a, dim = 2)
-allocate(leftmaximum(N))
-allocate(rightmaximum(N))
-allocate(downmaximum(N))
-allocate(smaximum(N))
+M = size(a, dim = 1)
+N = size(a, dim = 2)
+allocate(leftmaximum(M))
+allocate(rightmaximum(M))
+allocate(downmaximum(M))
+allocate(smaximum(M))
 !$omp parallel shared(a, M, N, smaximum, leftmaximum, rightmaximum, downmaximum) private(MoveUp, MoveDown, MoveLeft, MoveRight, summ, p) default(none)
-allocate(p(M))
+allocate(p(N))
 !$omp do schedule(dinamic)
-do MoveUp = 1,N
+do MoveUp = 1,M
   p = 0
-  do MoveDown = MoveUp,N
-    do j = 1,M
+  do MoveDown = MoveUpm,
+    do j = 1,N
       p(j) = p(j) + a(MoveDown, j)
     enddo
     call SumArray(p, MoveLeft, MoveRight, summ)
@@ -38,7 +38,7 @@ enddo
 deallocate(p)
 !$omp end parallel
 max = smaximum(1); x1 = 1; y1 = leftmaximum(1); x2 = downmaximum(1); y2 = rightmaximum(1);
-do j=2,N
+do j=2,M
   if (smaximum(j) > max) then
     max = smaximum(j); x1 = j; y1 = leftmaximum(j); x2 = downmaximum(j); y2 = rightmaximum(j);
   endif
@@ -58,7 +58,7 @@ real(8) :: maximum, sum1, sum2
 M = size(a)
 sum = a(1); x1 = 1; x2 = 1
 maximum=a(1); l=1
-do i=2,M
+do i=2,N
   sum1 = a(i)
   sum2 = maximum + a(i)
   if (sum1 > sum2) then
